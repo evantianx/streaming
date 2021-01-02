87ea4935712cb6d498e17c78c7ec8a93ac9e4217
evantianx <evantianx@gmail.com>
2021-01-02 10:29:34 +0800

    feat: ThemeProvider of App
    Signed-off-by: evantianx <evantianx@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import App from "next/app"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider } from "@material-ui/core/styles"
+import { themeDark } from "lib/theme"
 
-const app = ({ Component, pageProps }) => <Component {...pageProps} />
+const app = ({ Component, pageProps }) => (
+  <ThemeProvider theme={themeDark}>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>
+)
 
 export default app
